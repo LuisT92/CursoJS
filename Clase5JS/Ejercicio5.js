f3880e574beeb2f8b6fbb95b13009aa9b61f91e9
Luis Humberto Torres <luis_acc92@hotmail.com>
2023-02-14 00:01:48 -0600

    Ejercicio 5
    Se trabaja con numero y sus métodos

---
+++ Clase5JS/Ejercicio5.js
@@ -0,0 +1,15 @@
+let altura_cm = 170;
+
+let altura_m = 1.70;
+
+let peso_kg = 90.7;
+
+
+let alt_m_roundup = Math.round(altura_m);
+console.log(alt_m_roundup);
+
+let peso_kg_roundDown = Math.floor(peso_kg);
+console.log(peso_kg_roundDown);
+
+let valMaxJS = (Number.MAX_VALUE) + 1;
+console.log(valMaxJS);
